194e0a231c50601ea163aaf7c1058f7a9a2690ea
Priyank R Diyora <az.priyankdiyora@gmail.com>
2026-05-18 16:04:40 +0530

    Update Software Developer Trainee duration to April 2026

--- utils/data/experience.js
+++ utils/data/experience.js
@@ -3,7 +3,7 @@ export const experiences = [
     id: 1,
     title: 'Software Developer Trainee',
     company: "Vision Infotech",
-    duration: "(Apr 2025 - March 2026)",
+    duration: "(Apr 2025 - April 2026)",
     tools: ['Java', 'Spring Boot', 'RESTful APIs', 'JWT', 'RBAC', 'MySQL', 'Git'],
     image: 'https://bunny-wp-pullzone-dth19nafvq.b-cdn.net/wp-content/uploads/2025/01/vision-new-header-logo.svg',
     description: 'Developed and maintained Java microservices using Spring Boot, exposing RESTful APIs to manage employee records. Implemented Role-Based Access Control (RBAC) with JWT authentication, enforcing least-privilege security policies. Optimized MySQL database schemas and query structures, delivering a 30% improvement in query performance.'
